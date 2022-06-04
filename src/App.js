b5109661e56d149df29979482d8d20ee15178e07
Harshvardhan <harshmandloi78@gmail.com>
2022-06-04 10:44:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react' ;
+
+import Greet from './components/Greet';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>the world is to large</h1>
+      
+    </React.Fragment>
+    
+  )
+}
+
+export default App
+
+
---
+++ src/components/Greet.js
@@ -0,0 +1,6 @@
+
+function Greet(){
+    return <h1>Hello World gftuyg!!</h1>
+}
+
+export default Greet
